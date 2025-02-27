16c149c644008cabbe91114bae3c1df70436900d
Luan-Rafael <luan.tec18@gmail.com>
2025-02-27 00:59:16 -0300

    feat: protected contructor base entity

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -3,7 +3,8 @@ import { randomUUID } from "crypto"
 export class Entity<T> {
     public props: T
     public id: string
-    constructor(props: T, id?: string) {
+
+    protected constructor(props: T, id?: string) {
         this.props = props
         this.id = id ?? randomUUID()
     }
