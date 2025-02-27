e4f800c3fc72c0b9bb9716912a04cbd07fd1614c
Luan-Rafael <luan.tec18@gmail.com>
2025-02-27 00:59:30 -0300

    type: create type optional

---
+++ src/core/types/options.ts
@@ -0,0 +1,18 @@
+/**
+ * Make some property option an type
+ * 
+ * @example
+ * ```typescript
+ * type Post {
+ * id: string,
+ * name: string,
+ * email: string
+ * }
+ * 
+ * 
+ * Optional<Post, 'id | 'email'>
+ * ```
+ * 
+ */
+
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
