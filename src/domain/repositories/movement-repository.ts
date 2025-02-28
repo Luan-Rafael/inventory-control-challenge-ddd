70213bf170bb504ebadfa33382b3577ac78f6e1e
Luan-Rafael <luan.tec18@gmail.com>
2025-02-28 10:29:21 -0300

    fix: implements repository movement

--- src/domain/repositories/movement-repository.ts
+++ src/domain/repositories/movement-repository.ts
@@ -1,3 +1,12 @@
+import { UniqueIdentityId } from "@/core/entities/unique-identity-id";
+import { Movement } from "../entities/movement";
 import { IMovementRepositoryImpl } from "./movement-repository-impl";
 
-export class MovementRepository implements IMovementRepositoryImpl { }
+export class MovementRepository implements IMovementRepositoryImpl {
+    async save(movement: Movement): Promise<void> {
+        return
+    }
+    async getProductBalance(productId: UniqueIdentityId): Promise<number> {
+        return 1; 
+    }
+}
